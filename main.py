eed609c2e1696ed293b22a8d0c0aaf49df164d68
Owen7000 <77976026+Owen7000@users.noreply.github.com>
2022-09-16 08:00:44 +0100

    Displays ISS lat/lon

---
+++ main.py
@@ -0,0 +1,30 @@
+from github import Github
+import urllib.request, json, time, os
+
+
+g = Github("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")
+lat = []
+lon = []
+loops = 0
+while loops < 7200:
+    req = urllib.request.urlopen("http://api.open-notify.org/iss-now.json")
+    obj = json.loads(req.read())
+    lat.append(obj['iss_position']['latitude'])
+    lon.append(obj['iss_position']['longitude'])
+    print(obj['iss_position']['latitude'])
+    print(obj['iss_position']['longitude'])
+    loops = loops + 1
+    time.sleep(5)
+    try:
+        os.system("cls")
+    except:
+        os.system("clear")
+
+body = ''
+
+for x in lat:
+    body = body + lat[x] + "\t" + lon[x]
+
+repo = g.get_repo("Owen7000/ISS-Tracker")
+repo.create_pull(title="Data Dump", body=body)
+
